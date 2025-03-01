3ac4cd9afcfbd8355e469782c473681dd3d0693a
1mxtheus <167809482+1mxtheus@users.noreply.github.com>
2025-03-01 03:26:02 +0000

    OK!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,7 +19,63 @@ int main() {
   float pib2;
   int pontos_turisticos2;
 
-  // …ea: %.2f \n", area1);
+  // Exibe o título do jogo
+  printf("---------Super Trunfo---------\n");
+  printf("Bem-Vindo ao Jogo Super Trunfo\n");
+
+  // Entrada de dados da Carta 1
+  printf("\nCarta 1");
+  printf("\nInforme a sigla do estado da carta 1: ");
+  scanf(" %c", &estado1);
+
+  printf("Informe o codigo da carta 1: ");
+  scanf(" %5s", codigo1); 
+
+  printf("Informe o nome da cidade da carta 1: ");
+  scanf(" %49s", nome_da_cidade1);
+
+  printf("Informe a populacao da carta 1: ");
+  scanf("%d", &populacao1);
+
+  printf("Informe a area da carta 1 (em km²): ");
+  scanf("%f", &area1);
+
+  printf("Informe o PIB da carta 1: \n");
+  scanf("%f", &pib1);
+
+  printf("Informe o numero de pontos turisticos da carta 1: \n");
+  scanf("%d", &pontos_turisticos1);
+
+  // Entrada de dados da Carta 2
+  printf("\nCarta 2");
+  printf("\nInforme a sigla do estado da carta 2: ");
+  scanf(" %c", &estado2);
+
+  printf("Informe o codigo da carta 2: \n");
+  scanf(" %5s", codigo2);
+
+  printf("Informe o nome da cidade da carta 2: \n");
+  scanf(" %49s", nome_da_cidade2);
+
+  printf("Informe a populacao da carta 2: \n");
+  scanf("%d", &populacao2);
+
+  printf("Informe a area da carta 2 (em km²): \n");
+  scanf("%f", &area2);
+
+  printf("Informe o PIB da carta 2: \n");
+  scanf("%f", &pib2);
+
+  printf("Informe o numero de pontos turisticos da carta 2: \n");
+  scanf("%d", &pontos_turisticos2);
+
+  // Exibir os dados da Carta 1
+  printf("\n===== Carta 1 =====\n");
+  printf("Estado: %c\n", estado1);
+  printf("Codigo: %s\n", codigo1);
+  printf("Nome da Cidade: %s\n", nome_da_cidade1);
+  printf("Populacao: %d\n", populacao1);
+  printf("Area: %.2f \n", area1);
   printf("PIB: %.2f \n", pib1);
   printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
 
